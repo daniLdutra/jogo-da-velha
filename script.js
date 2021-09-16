f339feb89965b9333a745fd1b49f47bd102b696b
Daniella Dutra <danivetdutra@gmail.com>
2021-09-16 12:38:21 -0300

    variaveis globais declaradas

---
+++ script.js
@@ -0,0 +1,8 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let container = document.querySelector("#container");
+let secondPlayer;
