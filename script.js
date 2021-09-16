83bc87ada8189a4cc82dfa7e2cc585fd7bea87de
Daniella Dutra <danivetdutra@gmail.com>
2021-09-16 12:43:05 -0300

    add contador de jogadas e evento click nos boxes

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let container = document.querySelector("#container");
 let secondPlayer;
+
+// contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+// adicionando o evento de click a todos as caixas
+for (let i = 0; i < boxes.length; i++) {
+  // quando há o click na caixa
+  boxes[i].addEventListener("click", function () {
+    let el = checkEl(player1, player2);
+
+    // verificando se ja tem um elemento
+    if (this.childNodes.length == 0) {
+      let cloneEl = el.cloneNode(true);
+
+      this.appendChild(cloneEl);
+
+      // computa a jogada
+      if (player1 == player2) {
+        player1++;
+      } else {
+        player2++;
+      }
+      //checa quem venceu
+      checkWinCondition();
+    }
+  });
+}
